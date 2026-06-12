3f028c58a45b77facd65d9872bf81dea06e6463a
suliman12xz-hub <suliman12xz@gmail.com>
2026-06-12 21:02:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/chat", methods=["POST"])
-def chat():
-    data = request.json
-    message = data.get("message", "")
-
-    return jsonify({"reply": "TEST OK: " + message})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
